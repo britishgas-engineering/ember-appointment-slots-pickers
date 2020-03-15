70379e9a7c5018d18e0a4e59ad9c1381471b339b
Leooo <viet_lionel@yahoo.fr>
2020-03-15 16:25:23 +0000

    fix eslint

--- addon/components/bg-button/index.js
+++ addon/components/bg-button/index.js
@@ -1,34 +1,8 @@
-//import {or} from '@ember/object/computed';
-//import {computed} from '@ember/object';
 import Component from '@glimmer/component';
-//import layout from './template';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { oneWay } from '@ember/object/computed';
-// import {
-//   layout as templateLayout,
-// } from '@ember-decorators/component';
-// import layout from './template';
 
-// @templateLayout(layout)
 export default class BgButtonComponent extends Component {
-  //layout = layout;
-  //layout,
-
-  //tagName: 'button',
-  //classNames: ['bg-button', 'btn', 'ember-appointment-slots-pickers'],
-  //classNameBindings: ['bgTheme'],
-  //attributeBindings: ['isDisabled:disabled', 'type', 'tabindex'],
-
-  // get isTagged() {
-  //   return this.args.isTagged || false;
-  // }
-
-  // attributes
-  // whether the button is disabled or not
-  // get disabled() {
-  //   return this.args.disabled || false;
-  // }
 
   // whether the button is loading state or not
   // can not use @trakced with #loadingPrivate, and @tracked is needed for isDisabled template updating test
@@ -37,11 +11,6 @@ export default class BgButtonComponent extends Component {
   get isLoading() {
     return this.args.loading || this.loadingPrivate;
   }
-
-  // //force 2-ways binding, not quite sure how to do differently (lack of @oneWay computed macro etc.)
-  // set isLoading(loading) {
-  //   this.args.loading = loading;
-  // }
 
   // can be "primary" "secondary" "tertiary"
   get theme() {
@@ -58,26 +27,12 @@ export default class BgButtonComponent extends Component {
      return this.args['loading-text'] || 'Loading...';
   }
 
-  // wheter to hide the left arrow icon
-  // get 'hide-icon'() {
-  //    return this.args['hide-icon'] || false;
-  // }
-
-  // get customIcon() {
-  //   return this.args.customIcon || null;
-  // }
-  //
-  // get forceLoadingToPersist() {
-  //   return this.args.forceLoadingToPersist || null;
-  // }
-
   get bgTheme() {
     return `btn-${this.theme}`;
   }
 
   constructor() {
     super(...arguments);
-    console.log('constructor', this, this.args);
   }
 
   //computed properties of disable and loading
