57e3963cc48fcbf9d9b669614a41551fd6d8af62
veerawb <balasaravanan.veeraswamy@centrica.com>
2020-08-07 20:00:31 +0530

    new component for BTM-860

---
+++ addon/components/slots-picker/desktop-no-category-column/component.js
@@ -0,0 +1,8 @@
+import layout from './template';
+import { computed } from '@ember/object';
+import slotPickerBase from 'ember-appointment-slots-pickers/components/slots-picker/desktop/component';
+
+export default slotPickerBase.extend({
+  layout
+
+});
