f5b855c0271ce0333673f5042be510225a4d2338
veerawb <balasaravanan.veeraswamy@centrica.com>
2020-08-10 16:13:13 +0530

    test case fix

--- addon/components/slots-picker/desktop-no-category-column/component.js
+++ addon/components/slots-picker/desktop-no-category-column/component.js
@@ -1,8 +1,6 @@
 import layout from './template';
-import { computed } from '@ember/object';
 import slotPickerBase from 'ember-appointment-slots-pickers/components/slots-picker/desktop/component';
 
 export default slotPickerBase.extend({
   layout
-
 });
--- tests/integration/components/slots-picker/desktop-no-category-column/component-test.js
+++ tests/integration/components/slots-picker/desktop-no-category-column/component-test.js
@@ -1,26 +1,105 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import {
+  module} from 'qunit'; import {setupRenderingTest
+} from 'ember-qunit';
+import { test } from 'qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { generateAppointmentSlots } from 'ember-appointment-slots-pickers/test-support/helpers/generate-appointment-slots';
+import { run } from '@ember/runloop';
+import { render, settled, findAll } from '@ember/test-helpers';
 
-module('Integration | Component | slots-picker/desktop-no-category-column', function(hooks) {
+module('Integration | Component | slots-picker/desktop-no-category-column', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function () {
+    this.actions = {};
+    this.send = (actionName, ...args) => this.actions[actionName].apply(this, args);
+  });
 
-    await render(hbs`{{slots-picker/desktop-no-category-column}}`);
+  test('with slots available, no slot selected', async function (assert) {
 
-    assert.equal(this.element.textContent.trim(), '');
+    generateAppointmentSlots.call(this, {
+      numberOfAppointments: 50
+    });
+    this.set('testSelect', () => {});
 
-    // Template block usage:
     await render(hbs`
-      {{#slots-picker/desktop-no-category-column}}
-        template block text
-      {{/slots-picker/desktop-no-category-column}}
+      <div>
+        {{#slots-picker
+          appointmentSlots=generatedAppointmentSlots
+          select=(action testSelect)
+          as |baseProps onSelectSlot|
+        }}
+          {{slots-picker/desktop-no-category-column
+            baseProps=baseProps
+            onSelectSlot=onSelectSlot
+          }}
+        {{/slots-picker}}
+      </div>
     `);
+   
+    assert.equal(findAll('.asp-btn').length > 0, true, 'has some available appointments loaded afterwards');
+    debugger;
+    assert.equal(findAll('.asp-col-header').length > 0, false, 'should not have category column');
+    assert.equal(findAll('.asp-scroll-btn-prev').length > 0, false, 'does not have previous button as shows the first appointments');
+    assert.equal(
+      findAll('.asp-col-header .asp-row.delimiter').length,
+      0,
+      'has no delimiter in the header column'
+    );
+    debugger;
+    assert.equal(
+      this.$('.asp-scroll .asp-col:eq(0) .asp-row.delimiter').length,
+      0,
+      'has no delimiter in the first non-header column'
+    );
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(findAll('.asp-row.asp-row-header.background-dark-blue').length > 0, true, 'default Brand background color has been applied');
+    assert.equal(
+      findAll('.asp-cell button').length,
+      this.get('availableAppointmentSlots.length'),
+      'has as many buttons as there are available slots');
+  });
+
+  test('with slots available, last slot selected', async function (assert) {
+
+    generateAppointmentSlots.call(this, {
+      numberOfAppointments: 50
+    });
+    this.actions.testSelect = () => {};
+    const generatedAppointmentSlots = this.get('generatedAppointmentSlots');
+    const selectedSlot = generatedAppointmentSlots[generatedAppointmentSlots.length - 1];
+    let availableAppointmentSlots;
+    run(() => {
+      selectedSlot.set('available', false);
+      availableAppointmentSlots = generatedAppointmentSlots.filterBy('available', true);
+    });
+    this.set('selected', selectedSlot);
+    await render(hbs`
+      <div>
+        {{#slots-picker
+          appointmentSlots=generatedAppointmentSlots
+          select=(action 'testSelect')
+          selected=selected
+          as |baseProps onSelectSlot|
+        }}
+          {{slots-picker/desktop-no-category-column
+            baseProps=baseProps
+            onSelectSlot=onSelectSlot
+          }}
+        {{/slots-picker}}
+      </div>
+    `);
+    return settled().then(() => {
+
+      assert.equal(findAll('.asp-btn').length > 0, true, 'has some available appointments loaded afterwards');
+      assert.equal(findAll('.asp-scroll-btn-prev').length > 0, true, 'has previous button');
+      assert.equal(findAll('.asp-scroll-btn-next').length > 0, false, 'does not have next button as shows the last appointments');
+
+      assert.equal(findAll('.asp-row.asp-row-header.background-dark-blue').length > 0, true, 'default Brand background color has been applied');
+      assert.equal(
+        findAll('.asp-cell button').length,
+        availableAppointmentSlots.get('length'),
+        'has as many buttons as there are available slots');
+    });
   });
 });
