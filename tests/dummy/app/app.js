9b36295e41a9d6465e6d95cb055f0aab8987f305
Paul Ashwin Abrham <abrahamsam@Pauls-MacBook-Pro.local>
2022-01-31 19:37:01 +0530

    Modified to up the application

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -9,17 +9,16 @@ export default class App extends Application {
   Resolver = Resolver;
 }
 
-if (config.environment === 'development') {
-  run.backburner.DEBUG = true;// slow
-  // Log errors potentially swallowed by promises
-  // (see https://guides.emberjs.com/v1.12.0/understanding-ember/debugging/)
-  on('error', function (error) {
-    console.error(error);//eslint-disable-line
-  });
-  Ember.onerror = function (error) {
-    console.error(error);//eslint-disable-line
-  };
-}
-
+// if (config.environment === 'development') {
+//   run.backburner.DEBUG = true;// slow
+//   // Log errors potentially swallowed by promises
+//   // (see https://guides.emberjs.com/v1.12.0/understanding-ember/debugging/)
+//   on('error', function (error) {
+//     console.error(error);//eslint-disable-line
+//   });
+//   Ember.onerror = function (error) {
+//     console.error(error);//eslint-disable-line
+//   };
+// }
 
 loadInitializers(App, config.modulePrefix);
